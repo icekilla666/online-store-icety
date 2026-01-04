0ed710e5d49c50d3a44ced32fc83e2d0fa54b24d
icekilla666 <mmitin2005@mail.ru>
2026-01-04 12:39:17 +0300

    CRUD для типов, брэндов, устройств + добавление статики

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,9 +1,32 @@
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class DeviceController {
-  async registration(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", fileName));
 
-  async create(req, res) {}
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        img: fileName,
+      });
 
-  async getAll(req, res) {}
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+
+  async getAll(req, res) {
+  }
 
   async getOne(req, res) {}
 }
